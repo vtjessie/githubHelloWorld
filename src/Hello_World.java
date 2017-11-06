91dd19939e58336a8f8df927b22807b509b68daf
vtjessie <32237821+vtjessie@users.noreply.github.com>
2017-11-06 16:33:43 +0530

    Created Hello World Sync on Github

---
+++ src/Hello_World.java
@@ -0,0 +1,12 @@
+
+public class Hello_World {
+
+	public Hello_World() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
